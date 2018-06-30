1c377cd49c3ad42710856a7f10e9f38230c58864
flo <flo.macicasan@gmail.com>
2018-06-30 11:49:42 +0300

    adds sorting for elements in a list

--- src/DemoFIles.java
+++ src/DemoFIles.java
@@ -1,7 +1,7 @@
 import fileoperation.FileOperationImpl1;
 import fileoperation.FileOperations;
 
-import java.util.List;
+import java.util.*;
 
 public class DemoFIles {
 
@@ -15,14 +15,52 @@ public class DemoFIles {
       //  System.out.println(poezie.size());
 
         int counter=0;
-        for (String vers: poezie
-             ) {
+        int versuriCuS = 0;
+        Map<Character, Integer> frequencies =new HashMap<>();
+        for (String vers: poezie) {
 
             if(!(vers.isEmpty() || vers.startsWith("...") || vers.startsWith("http")))
                 counter++;
+            if(vers.startsWith("S"))
+                versuriCuS++;
+
+
+//            frequencies = new HashMap<>();
+            for(int i = 0; i < vers.length(); i++) {
+                Character c = vers.charAt(i);
+                if(frequencies.containsKey(c)) {
+                    Integer frecventaCaracter = frequencies.get(c);
+                    frecventaCaracter++;
+                    frequencies.put(c, frecventaCaracter);
+                } else {
+                    frequencies.put(c, 1);
+                }
+            }
+
         }
+        System.out.println(frequencies);
+
+        ArrayList<Map.Entry<Character, Integer>> listOfFrequencies = new ArrayList<>(frequencies.entrySet());
+        Collections.sort(listOfFrequencies, new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return Integer.compare(o2.getValue(), o1.getValue());
+            }
+        });
+        System.out.println(listOfFrequencies);
+
+
+        TreeMap<Character, Integer> sorted = new TreeMap<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return Character.compare(o1, o2);
+            }
+        });
+        sorted.putAll(frequencies);
+        System.out.println(sorted);
 
         System.out.println(counter);
+        System.out.println("Versuri ce incep cu S:" + versuriCuS);
 
     }
 }
